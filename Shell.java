a3ec94f295e132bf15fce13fe048719e6d803f27
hengshuai yao <hengshuai@gmail.com>
2014-09-30 21:53:43 -0700

    style checked

--- Shell.java
+++ Shell.java
@@ -1,15 +1,17 @@
-public class Shell extends Sort{
+public class Shell extends Sort {
 
-    public void sort(Comparable[] a){
-        int h=1;
-        while(h<a.length) h=3*h+1;
+    public void sort(Comparable[] a) {
+        int h = 1; 
+        while (h < a.length/3) h = 3*h+1; 
 
-        while(h>=1){
-                for(int i=h;i<a.length;i++){
-                    for(int j=i; j>=h; j-=h){
-                        if(less(a[j], a[j-h])) exch(a,j,j-h);
+        while (h >= 1) {
+                for (int i = h; i < a.length; i++) {
+                //shell sort looks to the left
+                    for (int j = i;  j >= h;  j -= h) {
+                        if (less(a[j],  a[j-h])) exch(a, j, j-h); 
                     }
                 }
-                h /=3;
-    }
+                h /= 3; 
+            }
+        }
 }
